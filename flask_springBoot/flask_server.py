91c3d22d43763ce4c27f1ff61dcad0511d13b889
khs <69028828+happysang@users.noreply.github.com>
2022-03-15 18:24:40 +0900

    Update flask_server.py

--- flask_springBoot/flask_server.py
+++ flask_springBoot/flask_server.py
@@ -1,14 +1,18 @@
 from flask import Flask
-
-import urllib.request
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/tospring")
 def spring():
-    
     return "파이썬에서 작성한 내용"
-    
+
+@app.route('/')
+def user_juso():
+ 
+    temp = request.args.get('temp', "user")
+ 
+    return "이미지주소: " + temp
     
 if __name__ == '__main__':
     app.run(debug=False,host="127.0.0.1",port=5000)
